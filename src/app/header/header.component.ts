727823bf532d9b933e2fc14a2eb124056461dfa7
Meriem W. <meriem.ben@gmx.de>
2025-02-26 14:11:25 +0100

    Added FontAwesome icons to HeaderComponent & fixed imports

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  faSearch = faSearch;
 }
