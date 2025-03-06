42fb003923d8daa4b59d93025fe49f9d7f709fe3
Meriem W. <meriem.ben@gmx.de>
2025-03-06 11:50:00 +0100

    Modified home component and styles, updated catnavigation styles, and added sidenavigation component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
+import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CatnavigationComponent],
+  imports: [HeaderComponent, CatnavigationComponent, SidenavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
