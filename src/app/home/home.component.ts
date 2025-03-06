d39994cee1812afbc4a20d5931206bff4e71591b
Meriem W. <meriem.ben@gmx.de>
2025-03-06 10:50:08 +0100

    Updated home module, modified app component, and removed unused catnavigation & header components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CatnavigationComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HomeComponent],
+  imports: [
+    CommonModule,
+    HomeComponent,
+    HeaderComponent,
+    CatnavigationComponent,
+    FontAwesomeModule,
+  ],
 })
 export class HomeModule {}
