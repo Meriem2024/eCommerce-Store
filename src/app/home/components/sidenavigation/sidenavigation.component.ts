d0f20b0ac82c75c78e58487ae89798985e1f11fb
Meriem W. <meriem.ben@gmx.de>
2025-03-14 12:31:34 +0100

    Refactored sidenavigation component, removed unused spec files, and added new data structure

--- src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Category } from '../../types/category.type';
+import { CategoryService } from '../../services/category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenavigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidenavigation.component.html',
-  styleUrl: './sidenavigation.component.scss'
+  styleUrl: './sidenavigation.component.scss',
 })
 export class SidenavigationComponent {
+  categories: Category[] = [];
 
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+  }
+
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
 }
